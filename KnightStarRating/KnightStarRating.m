8a5789807435fde5dd4ab634442eab216d2f309d
JokerKin <weijoker_king@163.com>
2017-09-28 17:37:08 +0800

    初次提交

---
+++ KnightStarRating/KnightStarRating.m
@@ -0,0 +1,191 @@
+//
+//  KnightStarRating.m
+//  Knight_Assistant
+//
+//  Created by shanyou on 2017/9/28.
+//  Copyright © 2017年 闪游网络. All rights reserved.
+//
+
+#import "KnightStarRating.h"
+
+@interface KnightStarRating ()
+@property (nonatomic) CGColorRef backCGColor;
+@end
+
+CGFloat ED_DEFAULT_HALFSTAR_THRESHOLD = 0.6;
+
+@implementation KnightStarRating
+
+#pragma mark Init & dealloc
+- (void)setDefaultProperties
+{
+    self.maxRating = 5.0;
+    self.rating = 0.0;
+    self.horizontalMargin = 0;
+    self.displayMode = KnightStarRatingDisplayFull;
+    self.halfStarThreshold = ED_DEFAULT_HALFSTAR_THRESHOLD;
+    self.backgroundColor = [UIColor clearColor];
+}
+
+- (instancetype)initWithCoder:(NSCoder *)coder
+{
+    self = [super initWithCoder:coder];
+    if (self) {
+        [self setDefaultProperties];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setDefaultProperties];
+    }
+    return self;
+}
+
+- (void)drawRect:(CGRect)rect {
+    
+    CGRect bounds = self.bounds;
+    CGContextRef ctx = UIGraphicsGetCurrentContext();
+    // Fill background color
+    CGContextSetFillColorWithColor(ctx, self.backCGColor);
+    CGContextFillRect(ctx, bounds);
+    // Draw rating Images
+    CGSize starSize = self.selectImage.size;
+    for(NSInteger i = 0; i < self.maxRating; i++) {
+        [self.normalImage drawAtPoint:[self pointOfStarAtPosition:i highlighted:YES]];
+        
+        if (i < self.rating) {
+            CGContextSaveGState(ctx);
+            
+            if (i < self.rating &&  self.rating < i + 1) {
+                CGPoint starPoint = [self pointOfStarAtPosition:i highlighted:NO];
+                float difference = self.rating - i;
+                CGRect rectClip;
+                rectClip.origin = starPoint;
+                rectClip.size = starSize;
+                if (self.displayMode == KnightStarRatingDisplayHalf && difference < self.halfStarThreshold ) {    // Draw half star image
+                    rectClip.size.width /= 2.0;
+                }
+                else if  (self.displayMode == KnightStarRatingDisplayAccurate) {
+                    rectClip.size.width *= difference;
+                }
+                else {
+                    rectClip.size.width = 0;
+                }
+                if (rectClip.size.width > 0) {
+                    CGContextClipToRect( ctx, rectClip);
+                }
+            }
+            
+            [self.selectImage drawAtPoint:[self pointOfStarAtPosition:i highlighted:YES]];
+            CGContextRestoreGState(ctx);
+        }
+    }
+}
+
+#pragma mark Drawing
+- (CGPoint)pointOfStarAtPosition:(NSInteger)position highlighted:(BOOL)hightlighted
+{
+    CGSize size = hightlighted ? self.selectImage.size : self.normalImage.size;
+    NSInteger starsSpace = self.bounds.size.width - 2 * self.horizontalMargin;
+    
+    NSInteger interSpace = 0;
+    interSpace = self.maxRating - 1 > 0 ? (starsSpace - (self.maxRating) * size.width) / (self.maxRating - 1) : 0;
+    if(interSpace < 0)
+        interSpace = 0;
+    CGFloat x = self.horizontalMargin + size.width * position;
+    if( position > 0 )
+        x += interSpace * position;
+    CGFloat y = (self.bounds.size.height - size.height) / 2.0;
+    return CGPointMake(x  ,y);
+}
+#pragma mark Setters
+
+- (void)setBackgroundColor:(UIColor *)backgroundColor
+{
+    [super setBackgroundColor:backgroundColor];
+    self.backCGColor = backgroundColor.CGColor;
+}
+- (void)setReturnBlock:(KnightStarRatingReturnBlock)returnBlock
+{
+    _returnBlock = [returnBlock copy];
+    _delegate = nil;
+}
+- (void)setDelegate:(id<KnightStarRatingDelegate>)delegate
+{
+    _delegate = delegate;
+    _returnBlock = nil;
+}
+- (void)setRating:(float)rating
+{
+    _rating = rating;
+    [self setNeedsDisplay];
+}
+- (void)setDisplayMode:(KnightStarRatingDisplayMode)displayMode
+{
+    _displayMode = displayMode;
+    [self setNeedsDisplay];
+}
+
+
+#pragma mark touch
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    if (!self.editable) {
+        return;
+    }
+    UITouch *touch = [touches anyObject];
+    CGPoint touchLocation = [touch locationInView:self];
+    self.rating = [self starsForPoint:touchLocation];
+    [self setNeedsDisplay];
+}
+
+-(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    if (!self.editable) {
+        return;
+    }
+    UITouch *touch = [touches anyObject];
+    CGPoint touchLocation = [touch locationInView:self];
+    self.rating =[self starsForPoint:touchLocation];
+    [self setNeedsDisplay];
+}
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    if (!self.editable) {
+        return;
+    }
+    if (self.delegate && [self.delegate respondsToSelector:@selector(starsSelectionChanged:rating:)]) {
+        [self.delegate starsSelectionChanged:self rating:self.rating];
+    }
+    if (self.returnBlock) {
+        self.returnBlock(self.rating);
+    }
+}
+#pragma mark Mouse/Touch Interaction
+- (CGFloat)starsForPoint:(CGPoint)point
+{
+    float stars=0;
+    for (NSInteger i = 0; i < self.maxRating; i++) {
+        CGPoint p = [self pointOfStarAtPosition:i highlighted:NO];
+        
+        if (point.x > p.x) {
+            float increment = 1.0;
+            
+            if (self.displayMode == KnightStarRatingDisplayHalf) {
+                float difference = (point.x - p.x) / self.normalImage.size.width;
+                
+                if (difference < self.halfStarThreshold) {
+                    increment = 0.5;
+                }
+            }
+            stars += increment;
+        }
+    }
+    return stars;
+}
+
+@end
